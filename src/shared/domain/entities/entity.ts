148f30714df232be20fffd7bc43e032f77c4266d
Gabriel Torres <gabrielst328@gmail.com>
2025-06-26 16:31:41 -0300

    feat(shared): created shared module and base entity

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,23 @@
+import { v4 } from 'uuid';
+
+export class Entity<Props> {
+  private _id: string;
+  private _props: Props;
+
+  constructor(props: Props, id?: string) {
+    this._props = props;
+    this._id = id ?? v4();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get props(): Props {
+    return this._props;
+  }
+
+  updateProps(props: Props) {
+    if (props !== this._props) this._props = props;
+  }
+}
--- src/users/domain/entities/user-entity.ts
+++ src/users/domain/entities/user-entity.ts
@@ -6,3 +6,5 @@ export type UserProps = {
   height?: number;
   weight?: number;
 };
+
+export class UserEntity {}
